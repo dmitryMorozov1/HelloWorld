644615511c5298e1bed19b0cf6512e2de08aef54
NikitaVozisov <nikitavozisov2108@gmail.com>
2018-03-27 23:14:26 +0300

    Added buttons.
    It works like shit.

--- src/components/My_page/profile-info.vue
+++ src/components/My_page/profile-info.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-container fluid class="profileInfo elevation-5 pa-2">
-      <v-content class="mb-3 elevation-5 block" >
+      <v-content class="mb-3 elevation-5 block">
         <v-container fluid>
           <img v-bind:src="profileInfo.avatar" id="Photo" class="profilePhoto elevation-3"
                onclick="this.style.width = document.body.clientWidth+'px'"
@@ -10,6 +10,19 @@
           <div class="ageAndSex text-xs-left" style="color:#808080">{{profileInfo.age}} лет,
             {{profileInfo.sex}}
           </div>
+          <div class="buttons">
+              <v-btn v-if="!profileInfo.you_following && !profileInfo.owns"
+                     class="follow elevation-5 ml-3" right="true" small="true">Подписаться</v-btn>
+              <v-btn v-if="profileInfo.you_following && !profileInfo.owns"
+                     class="follow elevation-5 ml-3" right="true" small="true">Вы подписаны</v-btn>
+              <v-btn v-if="profileInfo.owns" class="follow elevation-5 ml-3" right="true"
+                     small="true">Редактировать</v-btn>
+              <v-btn class="options elevation-5 mr-3" left="true" small="true"
+                     block="true"><p class="opt">...</p></v-btn>
+          </div>
+          <p class="follows" v-if="profileInfo.follows===true">Подписан(а) на вас</p>
+          <p class="about text-xs-left heading">О себе</p>
+          <p class="aboutMe text-xs-left" style="color:#808080">{{profileInfo.about}}</p>
           <v-container class="links">
             <v-tooltip bottom v-for="link in profileInfo.social">
                <span slot="activator">
@@ -20,8 +33,6 @@
               <span>{{link}}</span>
             </v-tooltip>
           </v-container>
-          <p class="about text-xs-left heading">О себе</p>
-          <p class="aboutMe text-xs-left" style="color:#808080">{{profileInfo.about}}</p>
         </v-container>
       </v-content>
       <v-content class="mb-3 elevation-5 block">
@@ -32,7 +43,8 @@
             </p>
             <div class="stats-wrapper-row mb-2">
               <div class="stats-wrapper-column mr-2">
-                <div class="questions stats pa-1 mb-2">Вопросы <p>{{profileInfo.stats.questions}}</p></div>
+                <div class="questions stats pa-1 mb-2">Вопросы <p>
+                  {{profileInfo.stats.questions}}</p></div>
                 <div class="likes stats pa-1">Лайки <p>{{profileInfo.stats.likes}}</p></div>
               </div>
               <div class="stats-wrapper-column mr-2">
@@ -40,8 +52,10 @@
                 <div class="rating stats pa-1">Рейтинг <p>{{profileInfo.stats.rating}}</p></div>
               </div>
               <div class="stats-wrapper-column mr-2">
-                <div class="answers stats pa-1 mb-2">Ответы <p>{{profileInfo.stats.answers}}</p></div>
-                <div class="dislikes stats pa-1">Дизлайки <p>{{profileInfo.stats.dislikes}}</p></div>
+                <div class="answers stats pa-1 mb-2">Ответы <p>{{profileInfo.stats.answers}}</p>
+                </div>
+                <div class="dislikes stats pa-1">Дизлайки <p>{{profileInfo.stats.dislikes}}</p>
+                </div>
               </div>
             </div>
           </div>
@@ -51,7 +65,8 @@
         <v-container fluid>
           <p class="achievements text-xs-left heading" align="left">Достижения</p>
           <v-container class="achievements-wrapper left">
-            <img v-for="achievment in profileInfo.achievements" class="ma-1 achievement" v-bind:src="achievment.url">
+            <img v-for="achievment in profileInfo.achievements" class="ma-1 achievement"
+                 v-bind:src="achievment.url">
           </v-container>
         </v-container>
       </v-content>
@@ -64,8 +79,7 @@
     name: "profile-info",
     data() {
       return {
-        profileInfo: {
-        }
+        profileInfo: {}
       }
     },
     methods: {},
@@ -76,60 +90,63 @@
      * At least window.location.href -- is never used by me
      * */
     mounted() {
-      this.profileInfo={
-        id:"1",
-        name:"Елизавета Седова",
-        sex:"Ж",
-        age:"20",
-        about:"Студентка 3 курса национального исследовательского " +
+      this.profileInfo = {
+        id: "1",
+        name: "Елизавета Седова",
+        sex: "Ж",
+        age: "20",
+        about: "Студентка 3 курса национального исследовательского " +
         "университета информационных технологий, механики и " +
         "оптики",
-        avatar:"./static/profileImage.png",
-        stats:{
-          questions:"94",
-          shows:"123",
-          answers:"321",
-          likes:"94",
-          rating:"123",
-          dislikes:"321",
+        avatar: "./static/profileImage.png",
+        stats: {
+          questions: "94",
+          shows: "123",
+          answers: "321",
+          likes: "94",
+          rating: "123",
+          dislikes: "321",
         },
-        achievements:{
-          achievement1:{
-            url:"./static/facebook.png",
-            name:"name1"
+        achievements: {
+          achievement1: {
+            url: "./static/facebook.png",
+            name: "name1"
           },
-          achievement2:{
-            url:"./static/facebook.png",
-            name:"name2"
+          achievement2: {
+            url: "./static/facebook.png",
+            name: "name2"
           },
-          achievement3:{
-            url:"./static/facebook.png",
-            name:"name3"
+          achievement3: {
+            url: "./static/facebook.png",
+            name: "name3"
           },
-          achievement4:{
-            url:"./static/facebook.png",
-            name:"name4"
+          achievement4: {
+            url: "./static/facebook.png",
+            name: "name4"
           },
-          achievement5:{
-            url:"./static/facebook.png",
-            name:"name5"
+          achievement5: {
+            url: "./static/facebook.png",
+            name: "name5"
           },
-          achievement6:{
-            url:"./static/facebook.png",
-            name:"name6"
+          achievement6: {
+            url: "./static/facebook.png",
+            name: "name6"
           },
-          achievement7:{
-            url:"./static/facebook.png",
-            name:"name7"
+          achievement7: {
+            url: "./static/facebook.png",
+            name: "name7"
           },
         },
-        social:{
-          facebook:"https://vk.com/id224332750",
-          vk:"https://vk.com/id224332750",
-          twitter:"https://vk.com/id224332750",
-          ok:"https://vk.com/id224332750",
-          telegram:"https://vk.com/id224332750"
+        social: {
+          facebook: "https://vk.com/id224332750",
+          vk: "https://vk.com/id224332750",
+          twitter: "https://vk.com/id224332750",
+          ok: "https://vk.com/id224332750",
+          telegram: "https://vk.com/id224332750"
         },
+        you_following:false,
+        follows:false,
+        owns:false,
       }
       /*let photo=this.$el.getElementsByClassName("profilePhoto")[0];
       photo.onclick=this.changePhotoSize();*/
@@ -142,7 +159,7 @@
         mode: 'cors',
         cache: 'default'
       };
-      let profileUrl = window.location.href;
+      let profileUrl=window.location.href;
       console.log(this.name);
       fetch(profileUrl, options)
         .then(function (response) {
@@ -177,6 +194,7 @@
     border-radius: 5px;
     height: 50%;
   }
+
   .links {
     display: flex;
     flex-direction: row;
@@ -210,6 +228,7 @@
     width: 12%;
     height: 12%;
   }
+
   .linkImage {
     width: 100%;
     height: 100%;
@@ -221,30 +240,62 @@
     width: 33%;
     margin-left: 0px;
   }
+
   p {
     margin-bottom: 0px;
   }
-  .block{
-       background-color: #fafafa !important;
-       border-radius: 5px;
+
+  .block {
+    background-color: #fafafa !important;
+    border-radius: 5px;
   }
+  .buttons{
+    display: flex;
+    flex-direction: row;
+  }
+  .opt{
+    transform: translate(-0%,-20%) !important;
+    font-size: 40px;
+    font-weight: bold;
+
+  }
+  .options{
+    background-color: #fafafa !important;
+    border-radius: 5px;
+  }
+  .follow{
+    text-transform: none;
+    background-color: #fafafa !important;
+    border-radius: 5px;
+    margin-left: 0px;
+    margin-right: 0px;
+    font-weight: bold;
+  }
+  .follows{
+    text-align: center;
+    color:#808080;
+  }
+
+
   /*Fonts*/
-  .profileName,.ageAndSex,.heading,.aboutMe,.stats-wrapper{
+  .profileName, .ageAndSex, .heading, .aboutMe, .stats-wrapper,.options {
     font-family: 'Roboto', sans-serif;
   }
+
   .profileName {
     font-size: 20px;
   }
-  .ageAndSex{
+
+  .ageAndSex {
     font-size: 16px;
   }
-  .heading{
+
+  .heading {
     font-size: 18px;
   }
-  .aboutMe{
+
+  .aboutMe,.stats-wrapper {
     font-size: 14px;
   }
-  .stats-wrapper{
-    font-size: 14px;
-  }
+
 </style>
