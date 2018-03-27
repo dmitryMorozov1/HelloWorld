88b4da9bedf14fee10dbb6b2fd5ed7f04ffa2111
kareni3 <36031205+kareni3@users.noreply.github.com>
2018-03-27 23:53:05 +0300

    [fix] changed to retrieve data

--- src/components/My_page/following.vue
+++ src/components/My_page/following.vue
@@ -16,7 +16,19 @@ import follower from './follower.vue'
 export default {
   data() {
     return {
-      peoples: [
+      following: []
+    }
+  },
+  components: {
+    'follower': follower
+  },
+  props: ['search'],
+  mounted () {
+    this.addfollowing();
+  },
+  computed: {
+    addfollowing() {
+      this.following = [
         { id: '1', name: 'Петя Говядко', link: '/Володя', sub: true, sig: true, img: "http://memesmix.net/media/created/m6tnhm.jpg"},
         { id: '2', name: 'Петя Говядко', link: '/Петя', sub: true, sig: false, img: "https://i.ytimg.com/vi/3v3tRVmgcZU/hqdefault.jpg"},
         { id: '3', name: 'Костя Говядко', link: '/Костя', sub: false, sig: false, img: "http://www.ovsyanko.ru/kosta/myself.jpg"},
@@ -24,16 +36,10 @@ export default {
         { id: '5', name: 'Отдых Говядко', link: '/Отдых', sub: true, sig: false, img: "https://files.adme.ru/files/news/part_88/880410/14451510-2363018019-600-e95273f1dc-1484580123.jpg"},
         { id: '6', name: 'Прапор Говядко', link: '/Прапор', sub: false, sig: true, img: "http://fans.mybspn.com/files/2012/07/2029_show_image_NpAdvSinglePhotophp_1.jpg"},
         { id: '7', name: 'Петя Говядко', link: '/Петя', sub: true, sig: true, img: "https://i.ytimg.com/vi/3v3tRVmgcZU/hqdefault.jpg"}
-        ]
-    }
-  },
-  components: {
-    'follower': follower
-  },
-  props: ['search'],
-  computed: {
+      ];
+    },
     filteredList() {
-      return this.peoples.filter(post => {
+      return this.following.filter(post => {
         return post.name.toLowerCase().includes(this.search.toLowerCase())
       })
     }
