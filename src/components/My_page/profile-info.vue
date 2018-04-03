dd3d7f58dd82ae03f48ccd69e320c4b81c3aa854
Nikita Vozisov <nikitavozisov2108@gmail.com>
2018-04-04 01:54:04 +0300

    Refactor
    *Added v-for for statistics
    *Removed some elevation classes

--- src/components/My_page/profile-info.vue
+++ src/components/My_page/profile-info.vue
@@ -1,32 +1,31 @@
 <template>
   <v-app>
-    <v-content fluid class="pa-2 elevation-5 mb-3 blocklight">
+    <v-content fluid class="pa-2 border mb-3 blocklight">
       <v-flex justify-center class="border mb-0" @click.stop="showAvatar = true"
-         @onmouseout.stop="showAvatar = false">
-        <img v-bind:src="profileInfo.avatar" align="center" width="100%"
-             class="profilePhoto elevation-3">
+              @onmouseout.stop="showAvatar = false">
+        <img v-bind:src="profileInfo.avatar" align="center" width="100%">
       </v-flex>
-      <v-dialog v-model="showAvatar" max-width="750px" width="75%">
-          <img v-bind:src="profileInfo.avatar" width="100%" class="pa-0 ma-0">
+      <v-dialog v-model="showAvatar" max-width="500px" width="50%">
+        <img v-bind:src="profileInfo.avatar" width="100%" class="pa-0 ma-0">
       </v-dialog>
-      <v-content class="profileName text-xs-left med-20">{{profileInfo.name}}</v-content>
-      <v-layout justify-left class="text-xs-left med-16 textgrey-text">
+      <v-content class="text-xs-left med-20">{{profileInfo.name}}</v-content>
+      <v-layout class="text-xs-left med-16 textgrey-text">
         {{profileInfo.age}} лет,
         {{profileInfo.sex}}
       </v-layout>
       <v-btn color="blue lighten-2 ma-0"
              v-if="!profileInfo.you_following && !profileInfo.owns"
-             class="follow elevation-0 med-16 border" round>Подписаться
+             class="follow med-16 border" style="height: 31px;" round depressed>Подписаться
       </v-btn>
       <v-btn v-if="profileInfo.you_following && !profileInfo.owns"
-             class="following elevation-0 ma-0 med-16 border" color="light-green lighten-4" round>Вы
-        подписаны
+             class="following ma-0 med-16 border" style="height: 31px;"
+             color="light-green lighten-4" round depressed>Вы подписаны
       </v-btn>
-      <v-btn v-if="profileInfo.owns" class="edit elevation-0 ma-0 med-16 border"
-             icon color="grey lighten-2" round>Редактировать
+      <v-btn v-if="profileInfo.owns" class="edit ma-0 med-16 border"
+             color="grey lighten-2" round depressed style="width: 100%; height: 31px;">Редактировать
       </v-btn>
-      <v-btn class="options elevation-0 pa-0 ma-0 border" round icon color="grey lighten-2"><i
-        class="material-icons">more_horiz</i>
+      <v-btn v-if="!profileInfo.owns" class="blocklight options pa-0 ma-0 border" round depressed
+             icon flat><i class="material-icons">more_horiz</i>
       </v-btn>
       <v-layout justify-center class="follows textgrey-text"
                 v-if="profileInfo.follows===true">
@@ -46,46 +45,21 @@
       </v-flex>
     </v-content>
 
-    <v-container fluid class="pb-0 pt-2 pr-2 pl-2 elevation-5 mb-3 blocklight">
-      <v-layout justify-left class="statistics text-xs-left thin-18" style="margin-bottom: 5px;">
+    <v-container fluid class="pb-0 pt-2 pr-2 pl-2 border mb-3 blocklight">
+      <v-layout class="statistics text-xs-left thin-18">
         Статистика
       </v-layout>
-      <v-layout row class="mb-2">
-        <v-layout column class="mr-2">
-          <v-layout align-center column class="questions stats border textgrey-text pa-1 mb-2">
-            Вопросы
-            <v-content>{{profileInfo.stats.questions}}</v-content>
-          </v-layout>
-          <v-layout align-center column class="likes stats border textgrey-text pa-1">
-            Лайки
-            <v-content>{{profileInfo.stats.likes}}</v-content>
-          </v-layout>
-        </v-layout>
-        <v-layout column class="mr-2">
-          <v-layout align-center column class="shows stats border textgrey-text pa-1 mb-2">
-            Показы
-            <v-content>{{profileInfo.stats.shows}}</v-content>
-          </v-layout>
-          <v-layout align-center column class="rating stats border textgrey-text pa-1">
-            Рейтинг
-            <v-content>{{profileInfo.stats.rating}}</v-content>
-          </v-layout>
-        </v-layout>
-        <v-layout column class="mr-2">
-          <v-layout align-center column class="answers stats border textgrey-text pa-1 mb-2">
-            Ответы
-            <v-content>{{profileInfo.stats.answers}}</v-content>
-          </v-layout>
-          <v-layout align-center column class="dislikes stats border textgrey-text pa-1">
-            Дизлайки
-            <v-content>{{profileInfo.stats.dislikes}}</v-content>
-          </v-layout>
+      <v-layout wrap row class="mb-2">
+        <v-layout style="width: 25%" v-for="(stat, index) in profileInfo.stats" :key="stat"
+                  align-center column class="border textgrey-text pa-1 mb-2 mr-2">
+          {{categories[index]}}
+          <v-content>{{stat}}</v-content>
         </v-layout>
       </v-layout>
     </v-container>
 
-    <v-container fluid class="pt-2 elevation-5 mb-2 pb-0 pl-2 blocklight">
-      <v-content class="achievements text-xs-left thin-18" align="left">Достижения</v-content>
+    <v-container fluid class="pt-2 border mb-2 pb-0 pl-2 blocklight">
+      <v-content class="text-xs-left thin-18" align="left">Достижения</v-content>
       <v-flex class="achievements-wrapper left med-14">
         <img v-for="(achievement, index) in profileInfo.achievements" class="ma-1 achievement"
              v-bind:src="achievement.url">
@@ -101,7 +75,15 @@
       return {
         profile: {},
         showAvatar: false,
-        links: []
+        links: [],
+        categories: {
+          questions: "Вопросы",
+          likes: "Лайки",
+          shows: "Показы",
+          rating: "Рейтинг",
+          answers: "Ответы",
+          dislikes: "Дизлайки",
+        },
       }
     },
     methods: {},
@@ -114,6 +96,7 @@
       this.links[2] = 'http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c53e.png';
       this.links[3] = 'https://upload.wikimedia.org/wikipedia/commons/0/09/Ok_new_logo.png';
       this.links[4] = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2000px-Telegram_logo.svg.png';
+
       switch (this.profileInfo.sex) {
         case "1":
           this.profileInfo.sex = "М";
@@ -125,8 +108,8 @@
           this.profileInfo.sex = "";
           break;
       }
-      this.profileInfo.follows = true;
-      this.profileInfo.you_following = false;
+      this.profileInfo.follows = false;
+      this.profileInfo.you_following = true;
       this.profileInfo.owns = false;
     }
 
