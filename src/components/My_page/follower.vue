1dca04d5ad393fab93188cc4e1353b7542a62ef8
kareni3 <36031205+kareni3@users.noreply.github.com>
2018-04-03 19:04:58 +0300

    [add] new styles

--- src/components/My_page/follower.vue
+++ src/components/My_page/follower.vue
@@ -1,13 +1,16 @@
 <template>
-  <v-layout px-2 my-0 elevation-3 class="follower" id="follower-comp">
-        <v-avatar>
-          <img :src="personData.img" alt="avatar">
-        </v-avatar>
-        {{ personData.name }}
-        <v-spacer></v-spacer>
-      <v-btn disabled v-if="personData.sig" class="sig"> Подписан на вас </v-btn>
-      <v-btn v-if="sub" @click="unfollow" class="issub"> Вы подписаны </v-btn>
-      <v-btn v-else @click="follow" class="nosub"> Подписаться </v-btn>
+  <v-layout px-2 my-0 class="border blocklight med-16" id="follower-comp">
+    <v-avatar>
+      <img :src="personData.img" alt="avatar">
+    </v-avatar>
+    {{ personData.name }}
+    <v-spacer></v-spacer>
+    <v-btn round disabled class="sig med-16 border"
+      v-if="personData.sig"> Подписан на вас </v-btn>
+    <v-btn round class="issub med-16 blockgreen border"
+      v-if="sub" @click="unfollow"> Вы подписаны </v-btn>
+    <v-btn round class="nosub med-16 blocklight-text blockblue border"
+      v-else @click="follow"> Подписаться </v-btn>
   </v-layout>
 </template>
 
@@ -35,14 +38,10 @@ export default {
 
 <style scoped>
 #follower-comp {
-  font-size: 16px;
-  /*------*/
   align-items: center;
-  background-color: #fff;
-  border-radius: 5px;
+  border-radius: 2px;
   width: 100%;
   min-height: 58px;
-  box-shadow: 0 1px 2px rgba(0,0,0,0.2) !important;
 }
 #follower-comp .avatar {
   height: 47px !important;
@@ -51,24 +50,17 @@ export default {
 }
 #follower-comp .btn {
   text-transform: inherit;
-  border-radius: 5px;
   height: 31px;
-  width: 142px;
+  width: 158px;
 }
 #follower-comp  .sig {
-  box-shadow: 0 0 1px rgba(0,0,0,0.2) !important;
-  background-color: #fefefe !important;
+  background-color: #fdfdfd !important;
   color: #000 !important;
 }
 #follower-comp .nosub {
-  box-shadow: 0 1px 2px rgba(0,0,0,0.2) !important;
-  background-color: #5cb1f2 !important; 
-  color: #fefefe;
   margin-right: 0;
 }
 #follower-comp .issub {
-  box-shadow: 0 1px 2px rgba(0,0,0,0.2) !important;
-  background-color: #ddf1ca !important;
   margin-right: 0;
 }
 </style>
