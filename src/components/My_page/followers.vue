98c06ce991c180c66df9e696f89e0cd773f6da0f
kareni3 <36031205+kareni3@users.noreply.github.com>
2018-04-04 02:59:35 +0300

    [add] paint the found text

--- src/components/My_page/followers.vue
+++ src/components/My_page/followers.vue
@@ -1,5 +1,5 @@
 <template>
-<v-container px-0 py-0>
+<v-container px-0 py-0 id="followers-comp">
   <search @inputsearch="inputsearch"></search>
   <v-container px-0 py-0 mb-2 
     v-for="person in filteredList"
@@ -25,7 +25,9 @@ export default {
   data() {
     return {
       followers: [],
-      search: ""
+      search: "",
+      copy_name: [],
+      cur_copy_name: []
     }
   },
   components: {
@@ -35,39 +37,72 @@ export default {
   mounted () {
     this.addfollowers;
   },
+  updated () {
+    this.coloringSearch();     
+  },
   computed: {
     addfollowers() {
       this.followers = [
         { id: '1', name: 'Петя Говядко', link: '/Володя', sub: true, sig: true, img: "http://memesmix.net/media/created/m6tnhm.jpg"},
         { id: '2', name: 'Петя Говядко', link: '/Петя', sub: true, sig: false, img: "https://i.ytimg.com/vi/3v3tRVmgcZU/hqdefault.jpg"},
         { id: '3', name: 'Костя Говядко', link: '/Костя', sub: false, sig: false, img: "http://www.ovsyanko.ru/kosta/myself.jpg"},
-        { id: '4', name: 'Лиза Говядко', link: '/Лиза', sub: true, sig: true, img: "http://www.ivoirebusiness.net/sites/default/files/poutine%20vladi_0.jpg"},
+        { id: '4', name: 'Лизгоа Говядко', link: '/Лиза', sub: true, sig: true, img: "http://www.ivoirebusiness.net/sites/default/files/poutine%20vladi_0.jpg"},
         { id: '5', name: 'Отдых Говядко', link: '/Отдых', sub: true, sig: false, img: "https://files.adme.ru/files/news/part_88/880410/14451510-2363018019-600-e95273f1dc-1484580123.jpg"},
         { id: '6', name: 'Прапор Говядко', link: '/Прапор', sub: false, sig: true, img: "http://fans.mybspn.com/files/2012/07/2029_show_image_NpAdvSinglePhotophp_1.jpg"},
         { id: '7', name: 'Петя Говядко', link: '/Петя', sub: true, sig: true, img: "https://i.ytimg.com/vi/3v3tRVmgcZU/hqdefault.jpg"}
       ];
     },
     filteredList() {
+      this.cur_copy_name = this.copy_name.filter(post => {
+        let str = this.search.trim();
+        return post.toLowerCase().includes(str.toLowerCase());
+      });
       return this.followers.filter(post => {
-        if(post.sig)
-        return post.name.toLowerCase().includes(this.search.toLowerCase());
-        else
-        return false;
+        if(post.sig) {
+          let str = this.search.trim();
+          return post.name.toLowerCase().includes(str.toLowerCase());
+        }
+        else {
+          return false;
+        }
       });
     }
   },
   methods: {
     inputsearch(input) {
       this.search = input.search;
+    },
+    coloringSearch() {
+      let list = document.getElementsByClassName("follower-fild-title");
+      for (let i = 0; i < list.length; i++) {
+        if(!!!this.copy_name[i]){
+          this.copy_name[i] = list[i].innerHTML;
+        }
+        if(!!this.cur_copy_name[i])
+          list[i].innerHTML = this.cur_copy_name[i];
+        else 
+          this.cur_copy_name[i] = list[i].innerHTML;
+        let str = this.search.trim();
+        str = list[i].innerHTML.match(new RegExp(str,"i"));
+
+        let result = list[i].innerHTML.match(new RegExp(str,"gi"));
+        result = result.filter(function(item, pos) {
+          return result.indexOf(item) == pos;
+        });
+        for (let k = 0; k < result.length; k++) {
+          list[i].innerHTML = 
+          list[i].innerHTML.replace(eval("/" + result[k] + "/g"),
+            "<span style='background:#c7dae8'>" + result[k] + "</span>"); 
+        }
+      }
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .followers {
   height: 58px;
   align-items: center;
-  font-size: 16px;
 }
 </style>
