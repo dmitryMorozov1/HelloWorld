e1a3dcdaafa23a414744f9712f73774348b94cd3
kir9819 <kirill9819@mail.ru>
2018-04-09 01:47:25 +0300

    add components for Top

---
+++ src/components/top/pages/top-day.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container class="border top blocklight">
+    
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.top {
+  height: 500px;
+}
+</style>
