b45cda6d4614586f8655f0d101886e37a051b312
kir9819 <kirill9819@mail.ru>
2018-04-10 11:12:37 +0300

    add page 'SelectQuestion' with components

---
+++ src/components/SelectQuestion/SQFriends.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container pa-0>
+    <v-flex mb-3 class="blocklight"></v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SelectQuestion/SelectQuestion.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container class="main-layout">
+    <v-layout>
+      <v-flex class="questions border blocklight">
+
+      </v-flex>
+      <v-flex>
+        <topics class="topics mr-0 mb-3 border blocklight"></topics>
+        <recommended-friends class="recommended-friends mr-0 border blocklight">
+        </recommended-friends>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import question from '../Main/question/question'
+import topics from './Topics'
+import recommendedFriends from './SQFriends'
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    'question' : question,
+    'topics' : topics,
+    'recommended-friends' : recommendedFriends
+  },
+  mounted() {
+    
+  }
+}
+</script>
+
+<style scoped>
+.questions {
+  width: 777px;
+  max-width: 777px;
+  height: 500px;
+}
+.topics {
+  width: 275px;
+  max-width: 275px;
+  height: 293px;
+}
+.recommended-friends {
+  width: 275px;
+  max-width: 275px;
+  height: 345px;
+}
+</style>
