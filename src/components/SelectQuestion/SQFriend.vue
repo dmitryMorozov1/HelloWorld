4ca3d90727bd6d4cf7ae40899ac5afd3c2a14b1c
Nikita Vozisov <nikitavozisov2108@gmail.com>
2018-04-15 00:58:13 +0300

    [fixed] Social Network Links

--- src/components/SelectQuestion/SQFriend.vue
+++ src/components/SelectQuestion/SQFriend.vue
@@ -6,14 +6,21 @@
         <img :src="friend.avatar">
       </v-list-tile-avatar>
 
+
       <v-list-tile-content>
         <v-list-tile-title class="ml-2">
           <a :href="'profile/'+friend.id" class="reg-15 black--text" target="_blank"
              style="text-decoration: none;">{{friend.name}}</a>
         </v-list-tile-title>
+
         <v-list-tile-sub-title class="reg-15 textdarkgrey-text mb-1 ml-2">
-          {{getSocialNetwork(friend)}}
+          <a v-if="friend.social!==0 && !friend.following" :href="'https://'+friend.linkSocial"
+             class="reg-15 textdarkgrey-text" target="_blank" style="text-decoration: none;">
+             {{getSocialNetwork(friend)}}
+          </a>
+          <v-layout v-else>{{getSocialNetwork(friend)}}</v-layout>
         </v-list-tile-sub-title>
+
 
       </v-list-tile-content>
       <v-icon class="pr-3" v-on:click="isAdded=!isAdded" style="cursor: pointer;" v-if="!isAdded">
@@ -43,32 +50,38 @@
 
     },
     methods: {
-      getSocialNetwork:(friend) => {
+      getSocialNetwork: (friend) => {
         if (friend.following) return "подписан(а) на вас";
         switch (friend.social) {
           case 0:
-            if (10 <= friend.commonFollowers && friend.commonFollowers <= 20){
-              return friend.commonFollowers+" общих друзей";
+            if (10 <= friend.commonFollowers && friend.commonFollowers <= 20) {
+              return friend.commonFollowers + " общих друзей"
             } else {
               let lastDigit = friend.commonFollowers % 10;
               switch (true) {
                 case lastDigit === 1:
-                  return friend.commonFollowers+" общий друг";
+                  return friend.commonFollowers + " общий друг";
                 case (2 <= lastDigit
                   && lastDigit <= 4):
-                  return friend.commonFollowers+" общих друга";
+                  return friend.commonFollowers + " общих друга";
                 case (5 <= lastDigit
                   && lastDigit <= 9):
                 case lastDigit === 0:
-                  return friend.commonFollowers+" общих друзей";
+                  return friend.commonFollowers + " общих друзей";
               }
             }
-          case 1: return "из Facebook";
-          case 2: return "из ВКонтакте";
-          case 3: return "из Twitter";
-          case 4: return "из Google+";
-          case 5: return "из Telegram";
-          case 6: return "из Badoo";
+          case 1:
+            return "из Facebook";
+          case 2:
+            return "из ВКонтакте";
+          case 3:
+            return "из Twitter";
+          case 4:
+            return "из Google+";
+          case 5:
+            return "из Telegram";
+          case 6:
+            return "из Badoo";
         }
       }
     }
