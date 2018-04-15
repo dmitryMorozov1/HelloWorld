8ed48cc210796739a59b94ad6495cce34da48b2a
kareni3 <36031205+kareni3@users.noreply.github.com>
2018-04-15 18:47:48 +0300

    [bug] routes TOP

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+// components
 import Favorites from '@/components/Favorites/Favorites'
 import Main from '@/components/Main/Main'
 import MyPage from '@/components/My_page/My_page'
 import News from '@/components/News/News'
 import Top from '@/components/Top/Top'
 import Error404 from '@/components/Main/404'
+import SelectQuestion from '@/components/SelectQuestion/SelectQuestion'
+
+// My_page
+import Answers from '@/components/My_page/answers'
+import Comments from '@/components/My_page/comments'
+import ProfileFavorites from '@/components/My_page/favorites'
+import Followers from '@/components/My_page/followers'
+import Following from '@/components/My_page/following'
+import Questions from '@/components/My_page/questions'
+
+// Top
+import TopDay from '@/components/Top/pages/top-day'
+import TopWeek from '@/components/Top/pages/top-week'
+import TopMonth from '@/components/Top/pages/top-month'
+import TopAll from '@/components/Top/pages/top-all'
+
 
 Vue.use(Router)
 
@@ -25,7 +43,40 @@ export default new Router({
     {
       path: '/profile/:id',
       name: 'Profile',
-      component: MyPage
+      component: MyPage,
+      children: [
+        {
+          path: 'answers',
+          name: 'ProfileAnswers',
+          component: Answers
+        },
+        {
+          path: 'comments',
+          name: 'ProfileComments',
+          component: Comments
+        },
+        {
+          path: 'favorites',
+          name: 'ProfileFavorites',
+          component: ProfileFavorites
+        },
+        {
+          path: 'followers',
+          name: 'ProfileFollowers',
+          component: Followers
+        },
+        {
+          path: 'following',
+          name: 'ProfileFollowing',
+          component: Following
+        },
+        {
+          path: 'questions',
+          name: 'ProfileQuestions',
+          component: Questions
+        }
+        
+      ]
     },
     {
       path: '/news',
@@ -35,7 +86,34 @@ export default new Router({
     {
       path: '/top',
       name: 'Top',
-      component: Top
+      component: Top,
+      children: [
+        {
+          path: 'day',
+          name: 'Top-day',
+          component: TopDay
+        },
+        {
+          path: 'week',
+          name: 'Top-week',
+          component: TopWeek
+        },
+        {
+          path: 'month',
+          name: 'Top-month',
+          component: TopMonth
+        },
+        {
+          path: 'all',
+          name: 'Top-all',
+          component: TopAll
+        }
+      ]
+    },
+    {
+      path: '/select_question',
+      name: 'SelectQuestion',
+      component: SelectQuestion
     },
     {
       path: '/*',
