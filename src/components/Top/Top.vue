06cd2ca7fae257f90eaed0a42234bfdb993382d9
kareni3 <36031205+kareni3@users.noreply.github.com>
2018-04-15 16:28:18 +0300

    [add] TOP components

--- src/components/Top/Top.vue
+++ src/components/Top/Top.vue
@@ -1,13 +1,54 @@
 <template>
-  
+  <v-container class="main-layout">
+    <v-layout>
+      <v-flex class="top">
+        <router-view :only-new-question="onlyNewQuestion"></router-view>
+      </v-flex>
+      <v-flex>
+        <top-menu  @showNewQuestions="showNewQuestions" class="top-menu mr-0 mb-3"></top-menu>
+        <top-popular class="top-popular mr-0"></top-popular>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import menu from './top-menu'
+import popular from './top-popular'
+
 export default {
-  
+  data() {
+    return {
+      onlyNewQuestion: false
+    }
+  },
+  components: {
+    'top-menu' : menu,
+    'top-popular' : popular
+  },
+  methods: {
+    showNewQuestions(input) {
+      this.onlyNewQuestion = input.tile;
+    }
+  },
+  created () {
+    //if (this.$route.name === 'Top')
+    //this.$router.push({name: 'Top-day'});
+  }
 }
 </script>
 
 <style scoped>
-
+.top-menu {
+  width: 275px;
+  max-width: 275px;
+}
+.top-popular {
+  width: 275px;
+  max-width: 275px;
+}
+.top {
+  width: 777px;
+  max-width: 777px;
+}
 </style>
