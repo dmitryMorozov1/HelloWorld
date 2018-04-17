f9a950b1cde4add81eaf7ae05afca5810583f2c0
kareni3 <36031205+kareni3@users.noreply.github.com>
2018-04-17 19:08:00 +0300

    [add] registration comp designed

---
+++ src/components/Main/Login/Registration.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card>
+    <v-flex class="text-xs-center py-2 med-24 textblue-text">
+      Регистрация
+    </v-flex>
+    <v-form 
+      id="form-registration-input-comp" 
+      v-model="valid" 
+      class="px-4">
+        <v-text-field
+          class="input"
+          label="Имя Фамилия"
+          v-model="name"
+          :rules="nameRules">
+        </v-text-field>
+        <v-text-field
+          class="input"
+          label="Email или телефон"
+          v-model="name"
+          :rules="nameRules">
+        </v-text-field>
+        <v-text-field
+          class="input"
+          label="Пароль"
+          v-model="email"
+          :rules="emailRules">
+        </v-text-field>
+    </v-form>
+    <v-flex class="text-xs-center py-3">
+      <v-btn 
+        color="white"
+        class="capitalize med-16 next-btn"
+        round
+        depressed
+        @click.native="next()">
+          Далее
+      </v-btn>
+    </v-flex>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return { }
+  },
+  methods: {
+    next() {
+      this.$emit('callPersonDataForm');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.next-btn {
+  min-width: 190px;
+  border: 1px solid #CACACA !important;
+}
+#form-registration-input-comp .input {
+  color: #1A93F0 !important;
+}
+</style>
