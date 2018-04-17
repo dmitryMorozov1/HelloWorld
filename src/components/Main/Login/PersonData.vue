24c0c7c0d33a8e4656d5353d2a414b641de258a4
kareni3 <36031205+kareni3@users.noreply.github.com>
2018-04-17 19:41:13 +0300

    [bug] colors

--- src/components/Main/Login/PersonData.vue
+++ src/components/Main/Login/PersonData.vue
@@ -4,13 +4,17 @@
       Регистрация
     </v-flex>
     <v-form 
-      v-model="valid" class="px-4">
+      id="form-person-data-input-comp"
+      v-model="valid" 
+      class="px-4">
         <v-text-field
+          class="input"
           label="Дата рождения"
           v-model="name"
           :rules="nameRules">
         </v-text-field>
         <v-text-field
+          class="input"
           label="Город"
           v-model="name"
           :rules="nameRules">
@@ -27,14 +31,14 @@
           <v-layout>
             <v-flex class="pl-4">
               <v-radio 
-                class="sex"
+                class="sex input"
                 label="Мужской" 
                 value="radio-1">
               </v-radio>
             </v-flex>
             <v-flex class="pl-4">
               <v-radio 
-                class="sex"
+                class="sex input"
                 label="Женский" 
                 value="radio-2">
               </v-radio>
@@ -78,6 +82,9 @@ export default {
 }
 .registration-btn {
   min-width: 190px;
-  border: 1px solid rgb(84, 144, 223) !important;
+  border: 1px solid #5DA3DA !important;
+}
+#form-person-data-input-comp .input {
+  color: #1A93F0 !important;
 }
 </style>
