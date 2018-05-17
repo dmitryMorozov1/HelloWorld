e97559c92a0e368d35a2ba296a31cf1a2ac62005
Nikita Vozisov <nikitavozisov2108@gmail.com>
2018-05-17 22:33:24 +0300

    [add] News initial commit

--- src/components/News/News.vue
+++ src/components/News/News.vue
@@ -1,13 +1,282 @@
 <template>
-  
+  <v-container class="main-layout page">
+    <v-layout>
+      <v-flex column class="questions">
+        <v-layout column v-for="question in questions">
+          <v-layout class="blocklight label textdarkgrey-text border pl-2">Сообщение по типу "25
+            пользователям понравился вопрос"
+          </v-layout>
+          <question :key="question.id" class="py-0 mb-4"
+                    :question="question"></question>
+        </v-layout>
+      </v-flex>
+      <v-flex>
+        <v-flex class="mr-0 ml-4 border blocklight best-questions right-block mb-3">
+          <v-flex class="pt-2 ">
+            <v-layout justify-center column class="reg-17 textdarkgrey-text mb-3 text-xs-center">
+              ЛУЧШИЕ ВОПРОСЫ
+              <v-layout column justify-center>
+                <v-list v-for="person in persons" dense class="pa-0 mb-0 blocklight">
+                  <v-divider class="ml-4 divider"></v-divider>
+                  <v-list-tile class="person mt-1 mb-2">
+                    <a :href="'profile/'+person.id" target="_blank">
+                      <v-list-tile-avatar>
+                        <img :src="person.avatar">
+                      </v-list-tile-avatar>
+                    </a>
+                    <v-list-tile-content>
+                      <v-layout class="reg-14 black--text">Here could be your amazing question</v-layout>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-layout>
+            </v-layout>
+          </v-flex>
+        </v-flex>
+
+        <v-flex class="mr-0 ml-4 border blocklight best-comments right-block mb-3">
+          <v-flex class="pt-2 ">
+            <v-layout justify-center column class="reg-17 textdarkgrey-text mb-3 text-xs-center">
+              ЛУЧШИЕ КОММЕНТАРИИ
+              <v-layout column justify-center>
+                <v-list v-for="person in persons" dense class="pa-0 mb-0 blocklight">
+                  <v-divider class="ml-4 divider"></v-divider>
+                  <v-list-tile class="person mt-1 mb-2">
+                    <a :href="'profile/'+person.id" target="_blank">
+                      <v-list-tile-avatar>
+                        <img :src="person.avatar">
+                      </v-list-tile-avatar>
+                    </a>
+                    <v-list-tile-content>
+                      <v-layout class="reg-14 black--text">Here could be your amazing comment or so</v-layout>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-layout>
+            </v-layout>
+          </v-flex>
+        </v-flex>
+
+        <v-flex class="mr-0 ml-4 border blocklight most-active right-block mb-3">
+          <v-flex class="pt-2 ">
+            <v-layout justify-center column class="reg-17 textdarkgrey-text mb-3 text-xs-center">
+              САМЫЕ АКТИВНЫЕ
+              <v-layout column justify-center>
+                <v-list two-line v-for="person in persons" dense class="pa-0 mb-0 blocklight">
+                  <v-divider class="ml-4 divider"></v-divider>
+                  <v-list-tile class="person">
+                    <a :href="'profile/'+person.id" target="_blank">
+                      <v-list-tile-avatar>
+                        <img :src="person.avatar">
+                      </v-list-tile-avatar>
+                    </a>
+                    <v-list-tile-content>
+                      <v-list-tile-title class="reg-14">вопросов: 16</v-list-tile-title>
+                      <v-list-tile-sub-title class="reg-14 black--text">комментариев: 31</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-layout>
+            </v-layout>
+          </v-flex>
+        </v-flex>
+
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  
-}
+  import question from '../Main/question/question'
+
+
+  export default {
+    data() {
+      return {
+        questions: [],
+        persons:[],
+      }
+    },
+    components: {
+      'question': question,
+    },
+    mounted() {
+
+      this.questions = [
+        {
+          id: 1,
+          ownerId: 1,
+          ownerName: 'Елизавета Седова',
+          avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg',
+          background: "https://img1.goodfon.ru/wallpaper/big/1/ca/foto-makro-kartinka-zelenye.jpg",
+          date: '29-03-2018 15:00:00.1111',
+          title: 'Меняет ли смена фамилии/имени судьбу человека?,lasd;lmcak ak kalksadk ;kslalkn aksjdlkanxkjasnxlanasnasnxasnxjsaxnajxalksjlaksdjaksd',
+          type: '1', // 1 - open, 2 - anonimous
+          like: 1, // 1 - like, 0 - nothing, -1 - dislike
+          favorite: false, // is favorite ?
+          answered: true, // is answered
+          watches: 920,
+          replies: 4,
+          likes: 456,
+          dislikes: 118,
+          tags: ['меняет', 'смена', 'фамилия', 'имя', 'судьба'],
+          answers: [
+            {
+              text: `Конечно меняет! Фамилия - это целый род,
+                   имеющий свою карму, как хорошую, так и плохую`,
+              replies: 3,
+              chosen: true,
+              friends: [
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                },
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                },
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                }
+              ]
+            },
+            {
+              text: ` Я вообще хз, что тут написать, левый вариант
+                    ответа я скопировала из интернета :)`,
+              replies: 1,
+              chosen: false,
+              friends: [
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 2,
+          ownerId: 1,
+          ownerName: 'Елизавета Седова',
+          avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg',
+          background: "https://img1.goodfon.ru/wallpaper/big/1/ca/foto-makro-kartinka-zelenye.jpg",
+          date: '29-03-2018 15:00:00.1111',
+          title: 'Меняет ли смена фамилии/имени судьбу человека?,lasd;lmcak ak kalksadk ;kslalkn aksjdlkanxkjasnxlanasnasnxasnxjsaxnajxalksjlaksdjaksd',
+          type: '1', // 1 - open, 2 - anonimous
+          like: 1, // 1 - like, 0 - nothing, -1 - dislike
+          favorite: false, // is favorite ?
+          answered: true, // is answered
+          watches: 920,
+          replies: 4,
+          likes: 456,
+          dislikes: 118,
+          tags: ['меняет', 'смена', 'фамилия', 'имя', 'судьба'],
+          answers: [
+            {
+              text: `Конечно меняет! Фамилия - это целый род,
+                   имеющий свою карму, как хорошую, так и плохую`,
+              replies: 3,
+              chosen: true,
+              friends: [
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                },
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                },
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                }
+              ]
+            },
+            {
+              text: ` Я вообще хз, что тут написать, левый вариант
+                    ответа я скопировала из интернета :)`,
+              replies: 1,
+              chosen: false,
+              friends: [
+                {
+                  id: 1,
+                  avatar: 'https://pp.userapi.com/c622120/v622120519/2607c/k0wPW3Pp__k.jpg'
+                }
+              ]
+            }
+          ]
+        }
+      ];
+      this.persons = [
+        {
+          id: 1,
+          name: 'Седова Елизавета',
+          avatar: 'https://wallpaperbrowse.com/media/images/pictures-2.jpg',
+          social: 0, // social id, 0 - no social, 1 - facebook, 2 - vkontakte, 3 - twitter, 4 - google+, 5 - telegram, 6 - badoo
+          linkSocial: 'https://google.com',
+          commonFollowers: 5, // only a number
+          following: false // is following to you
+        },
+        {
+          id: 2,
+          name: 'Седова Елизавета',
+          avatar: 'https://wallpaperbrowse.com/media/images/pictures-2.jpg',
+          social: 0, // social id, 0 - no social, 1 - facebook, 2 - vkontakte, 3 - twitter, 4 - google+, 5 - telegram, 6 - badoo
+          linkSocial: '',
+          commonFollowers: 5, // only a number
+          following: false // is following to you
+        },
+        {
+          id: 3,
+          name: 'Илья Говядко',
+          avatar: 'https://wallpaperbrowse.com/media/images/pictures-2.jpg',
+          social: 2, // social id, 0 - no social, 1 - facebook, 2 - vkontakte, 3 - twitter, 4 - google+, 5 - telegram, 6 - badoo
+          linkSocial: 'vk.com/id43376670',
+          commonFollowers: 0, // only a number
+          following: false // is following to you
+        },
+        {
+          id: 4,
+          name: 'Седова Елизавета',
+          avatar: 'https://wallpaperbrowse.com/media/images/pictures-2.jpg',
+          social: 0, // social id, 0 - no social, 1 - facebook, 2 - vkontakte, 3 - twitter, 4 - google+, 5 - telegram, 6 - badoo
+          linkSocial: '',
+          commonFollowers: 0, // only a number
+          following: true // is following to you
+        },
+        {
+          id: 5,
+          name: 'Илья Говядко',
+          avatar: 'https://wallpaperbrowse.com/media/images/pictures-2.jpg',
+          social: 0, // social id, 0 - no social, 1 - facebook, 2 - vkontakte, 3 - twitter, 4 - google+, 5 - telegram, 6 - badoo
+          linkSocial: '',
+          commonFollowers: 2, // only a number
+          following: false // is following to you
+        }
+      ];
+
+    }
+  }
 </script>
 
 <style scoped>
+  .right-block {
+    width: 275px;
+    max-width: 275px;
+    height: 300px;
+  }
+  .most-active{
+    height:340px;
+  }
+
+  .label {
+    width: 777px;
+    max-width: 777px;
+    height: 25px;
+  }
+  .divider{
+    width: 80%;
+  }
 
 </style>
