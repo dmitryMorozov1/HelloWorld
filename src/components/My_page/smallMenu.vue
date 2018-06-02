5d03829cfb6e2b9b86ee15b8a836e80ea5a73249
Nikita Vozisov <nikitavozisov2108@gmail.com>
2018-06-02 14:38:21 +0300

    [refactor] Lines are refactored

--- src/components/My_page/smallMenu.vue
+++ src/components/My_page/smallMenu.vue
@@ -3,28 +3,31 @@
     <v-content v-for="category in categories" class="ml-2 mt-2" :key="category.id">
       <a v-if="category.id===3" @click.stop="feedBack = true"
          @onmouseout.stop="feedBack = false" class="textdarkgrey-text categories">
-         {{category.name}}</a>
+        {{category.name}}</a>
 
       <v-dialog v-if="category.id===3" v-model="feedBack" max-width="500px" width="50%">
         <v-card class="pa-2">
 
-              <v-layout class="right">
-                <v-icon @click="feedBack=false" class="material-icons pointer">
-                  clear
-                </v-icon>
-              </v-layout>
-              <v-layout justify-center class="textblue-text bold-22 mt-2">Обратная связь</v-layout>
+          <v-layout class="right">
+            <v-icon @click="feedBack=false" class="material-icons pointer">
+              clear
+            </v-icon>
+          </v-layout>
+          <v-layout justify-center class="textblue-text bold-22 mt-2">Обратная связь</v-layout>
 
 
-            <v-flex class="ml-2">
-              <v-layout justify-left class="textblue-text med-18">Выберите тему</v-layout>
-              <v-layout justify-left column v-for="problem in problems" :key="problem.id">
-                <v-layout row>
-                  <v-checkbox color="blue" class="check" hide-details v-model="problem.checked" style="max-width: 30px;"></v-checkbox>
-                  <v-layout justify-left class="med-16 pointer" @click="problem.checked=!problem.checked">{{problem.name}}</v-layout>
+          <v-flex class="ml-2">
+            <v-layout justify-left class="textblue-text med-18">Выберите тему</v-layout>
+            <v-layout justify-left column v-for="problem in problems" :key="problem.id">
+              <v-layout row>
+                <v-checkbox color="blue" class="check" hide-details v-model="problem.checked"
+                            style="max-width: 30px;"></v-checkbox>
+                <v-layout justify-left class="med-16 pointer"
+                          @click="problem.checked=!problem.checked">{{problem.name}}
                 </v-layout>
               </v-layout>
-            </v-flex>
+            </v-layout>
+          </v-flex>
 
           <v-container class="px-2 pt-0 pb-0">
             <v-layout justify-left class="textblue-text med-16 mt-2 mb-0">Сообщение</v-layout>
@@ -76,7 +79,7 @@
           },
         ],
         feedBack: false,
-        problems:[
+        problems: [
           {
             id: 1,
             name: "Ошибка",
@@ -116,17 +119,19 @@
   .divider {
     width: 95%;
   }
-  .pointer{
+
+  .pointer {
     cursor: pointer;
   }
+
   .categories:hover {
     color: #1A93F0 !important;
   }
+
   .bigBtn {
     text-transform: none;
     width: 150px;
     height: 31px;
-    float:right;
+    float: right;
   }
-  
 </style>
